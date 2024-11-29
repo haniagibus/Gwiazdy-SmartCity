a6114f3a0f809ed0063985760ba15d0082c62002
haniagibus <hg.gibus@gmail.com>
2024-11-29 02:22:44 +0100

    noise pollution legend added

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ def hello():
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
+    #production server
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
+
